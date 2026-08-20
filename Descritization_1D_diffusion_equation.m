81e8302d82b9eb0b77577e689758096d90b25b84
Umme Salma Tasnim <us.tasnim@gmail.com>
2026-08-20 06:45:33 +0600

    Add files via upload

---
+++ Descritization_1D_diffusion_equation.m
@@ -0,0 +1,37 @@
+clear 
+close all
+clc
+
+%% define the mesh
+n_points = 5;
+domain_size = 1;
+h = domain_size/(n_points -1);
+%% Initializing the problem
+y(1) = 0;
+y(n_points) = 1;
+
+y_new(1) = 0;
+y_new(n_points) = 1;
+
+error_mag = 1;
+error_req = 1e-7;
+iterations = 0;
+%% Calculations
+while error_mag > error_req
+    for i=2:(n_points -1)
+          y_new(i)= 0.5 * (y(i-1)+ y(i+1));
+          iterations = iterations + 1;
+     end
+     % Calculations of error magnitude
+     error_mag = 0;
+     for i=2:(n_points -1)
+           error_mag = error_mag + abs(y(i)- y_new(i));
+     end
+     % assigning the new to be old
+     y = y_new;
+     
+end
+%% plotting
+x_dom = ((1:n_points)-1)*h;
+figure;
+plot(x_dom,y)
